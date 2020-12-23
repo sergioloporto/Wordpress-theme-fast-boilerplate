f01ea1c491c2efaedfe564a174c32b7437e214b9
Katie Hartrick <6202772+katiehartrick@users.noreply.github.com>
2020-12-23 13:52:13 +0800

    add dart-sass and stylelint

--- gulpfile.js
+++ gulpfile.js
@@ -1,62 +1,97 @@
-const gulp = require("gulp");
-const babel = require("gulp-babel");
-const sass = require("gulp-sass");
-const sourcemaps = require("gulp-sourcemaps");
-const autoprefixer = require("gulp-autoprefixer");
-const browserSync = require("browser-sync");
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+const sass = require('gulp-dart-sass');
+const stylelint = require('gulp-stylelint');
+const sourcemaps = require('gulp-sourcemaps');
+const autoprefixer = require('gulp-autoprefixer');
+const browserSync = require('browser-sync');
 const reload = browserSync.reload;
-const concat = require("gulp-concat");
-sass.compiler = require("node-sass");
+const concat = require('gulp-concat');
 
 gulp.task('babel', () =>
-    gulp.src('js/*.js')
-        .pipe(sourcemaps.init())
-        .pipe(babel({
-            presets: ['@babel/env']
-        }))
-        .pipe(concat('all.js'))
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('.'))
-        .pipe(reload({ stream: true }))
+  gulp.src('js/*.js')
+  .pipe(sourcemaps.init())
+  .pipe(babel({
+      presets: ['@babel/env']
+  }))
+  .pipe(concat('main.js'))
+  .pipe(sourcemaps.write('.'))
+  .pipe(gulp.dest('.'))
+  .pipe(reload({ stream: true }))
 );
 
-gulp.task("browser-sync", function() {
-  const files = ["./scss/*.scss", "./*.php", "./js/*.js",];
+gulp.task('browser-sync', function() {
+  const files = ['./scss/*.s+(a|c)ss', './*.php', './js/*.js',];
   browserSync.init(files, {
-    proxy: "http://newtheme:81",
+    proxy: 'https://yoursitename.local',
     notify: true
   });
-  gulp.watch("./scss/**/*.scss", gulp.series(css));
-  gulp.watch("./js/*.js"), gulp.series("babel");
-  gulp.watch("./*.php").on("change", browserSync.reload);
+  gulp.watch('./scss/**/*.s+(a|c)ss', gulp.series(css));
+  gulp.watch('./js/*.js'), gulp.series('babel');
+  gulp.watch('./*.php').on('change', browserSync.reload);
 });
 
 const css = function() {
   return gulp
-    .src("./scss/main.scss")
+    .src('./scss/**/*.s+(a|c)ss')
+    .pipe(stylelint({
+      reporters: [
+        {formatter: 'string', console: true}
+      ],
+      failAfterError: true,
+      console: true
+    }))
     .pipe(sourcemaps.init())
     .pipe(
       sass({
-        outputStyle: "compressed"
-      }).on("error", sass.logError)
+        outputStyle: 'expanded'
+      }).on('error', sass.logError)
     )
     .pipe(autoprefixer())
     .pipe(sourcemaps.write())
-    .pipe(concat("style.css"))
-    .pipe(gulp.dest("./"))
+    .pipe(concat('style.css'))
+    .pipe(gulp.dest('./'))
     .pipe(reload({ stream: true }));
 };
 
+const minify = function() {
+  return gulp
+    .src('./scss/**/*.s+(a|c)ss')
+    .pipe(stylelint({
+      reporters: [
+        {formatter: 'string', console: true}
+      ],
+      failAfterError: true,
+      console: true
+    }))
+    .pipe(
+      sass({
+        outputStyle: 'compressed'
+      }).on('error', sass.logError)
+    )
+    .pipe(autoprefixer())
+    .pipe(concat('style.css'))
+    .pipe(gulp.dest('./'))
+};
+
 const watch = function(cb) {
-  gulp.watch("./scss/**/*.scss", gulp.series(css));
-  gulp.watch("./js/*.js"), gulp.series("babel");
-  gulp.watch("./*.php").on("change", browserSync.reload);
+  gulp.watch('./scss/**/*.scss', gulp.series(css));
+  gulp.watch('./js/*.js'), gulp.series('babel');
+  gulp.watch('./*.php').on('change', browserSync.reload);
   cb();
 };
 
+gulp.task('vendors', function() {
+  return gulp.src('node_modules/@fortawesome/fontawesome-free/js/all.js')
+    .pipe(concat('all.js'))
+    .pipe(gulp.dest('vendors/'))
+    .pipe(reload({ stream: true }));
+});
+
 exports.css = css;
 exports.watch = watch;
-exports.default = gulp.series(css, "babel", watch, "browser-sync");
+exports.build = gulp.series(minify, 'babel', 'vendors');
+exports.default = gulp.series(css, 'babel', 'vendors', watch, 'browser-sync');
 
 // gulp.series // one by one
 // gulp.parallel // altogether
